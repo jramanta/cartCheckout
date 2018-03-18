45ab6a980b5feb460b38e7411ec1a31d4ab27b5a
Ioanna Ramanta <jramanta@gmail.com>
2018-03-18 18:01:33 +0000

    Addition of documentation on service methods

--- src/main/java/com/jramanta/cartCheckout/services/CartCheckoutService.java
+++ src/main/java/com/jramanta/cartCheckout/services/CartCheckoutService.java
@@ -11,6 +11,12 @@ import java.util.stream.Collectors;
 
 public class CartCheckoutService {
 
+    /**
+     * Calculates the total price of the given cart given the prices and offers of the available products.
+     *
+     * @param cartInput an input String in the format of products separated by a space character.
+     * @param availableProducts a map of all available products with key the products' code.
+     * */
     public static Double calculateCartPrice(String cartInput, Map<String, Product> availableProducts) {
         Map<String, Long> cart = buildUserCart(cartInput);
 
@@ -19,6 +25,9 @@ public class CartCheckoutService {
                 .mapToDouble(calculatePrice(availableProducts)).sum();
     }
 
+    /**
+     * Function implementation for product price calculation.
+     * */
     private static ToDoubleFunction<Map.Entry<String, Long>> calculatePrice(Map<String, Product> availableProducts) {
         return entry -> {
             Double price = 0d;
@@ -30,6 +39,11 @@ public class CartCheckoutService {
         };
     }
 
+    /**
+     * A map with key the product code and value the number of the product occurrences in the cart is built.
+     *
+     * @param cartInput our cart for checkout in the form of product codes separated by a space.
+     * */
     private static Map<String, Long> buildUserCart(String cartInput) {
         List<String> productCodes = Arrays.asList(cartInput.split(" "));
         return productCodes
@@ -37,23 +51,39 @@ public class CartCheckoutService {
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
+    /**
+     * The calculation of a product price based on the total product's occurrences.
+     * If the product has an offer we divide the product count by the offers number of items to find how many product's offers apply.
+     * The remainder of the division is the number of items that don't apply on the offer so the unit item price applies
+     * If the product has no offer then the product count is multiplied by the product unit's price.
+     *
+     * @param product one of the available products
+     * @param productCount the count of the product in the cart
+     * @return the total price for the input product after the offer has been applied
+     * */
     private static Double calculateProductPrice(Product product, long productCount) {
         Double productTotalPrice = 0d;
 
         if (product.getWeeklyOffer() != null) {
             long offerItems = product.getWeeklyOffer().getNumberOfItems();
-            long timesOfOffer = productCount / (product.getWeeklyOffer().getNumberOfItems());
+            // find numbers of offers
+            long timesOfOffer = productCount / offerItems;
             if (timesOfOffer > 0) {
+                // multiply the offer price by the offer count
                 productTotalPrice = timesOfOffer * product.getWeeklyOffer().getPrice();
             }
 
+            // calculate the remainder of the above division
             long productUnitRemainder = productCount % offerItems;
             if (productUnitRemainder > 0) {
+                // add to the total price the multiplication of the remainder with the product unit price
                 productTotalPrice = productTotalPrice + productUnitRemainder * product.getUnitPrice();
             }
         } else {
+            // if no offer applies multiply the product unit price with the product count
             productTotalPrice = product.getUnitPrice() * productCount;
         }
+
         return productTotalPrice;
     }
 
--- src/main/java/com/jramanta/cartCheckout/services/CommandService.java
+++ src/main/java/com/jramanta/cartCheckout/services/CommandService.java
@@ -7,6 +7,13 @@ import java.util.Map;
 
 public class CommandService {
 
+    /**
+     * Parses the input of the application user and translates it into a valid command or throws an exception if input is invalid
+     *
+     * @param input the input as provided by the application user
+     * @return a {@code Command}
+     * @throws NoSuchMethodException
+     * */
     public static Command parseCommand(String input) throws NoSuchMethodException {
         if (input != null && !input.isEmpty()) {
             for (Command commandValue : Command.values()) {
@@ -19,6 +26,13 @@ public class CommandService {
         throw new NoSuchMethodException();
     }
 
+    /**
+     * Applies the command implementation
+     *
+     * @param command the command type
+     * @param input the input as given from the application user
+     * @param availableProducts a map with key all available products' code and value the products themselves
+     * */
     public static void applyCommand(Command command, String input,
                                     Map<String, Product> availableProducts) throws NoSuchMethodException {
 
@@ -42,6 +56,9 @@ public class CommandService {
 
     }
 
+    /**
+     * The implementation of the HELP command. Prints out a list of the available valid commands of the system.
+     * */
     private static void printCommandGuidelines() {
         System.out.println("- Enter pricing update command in the format: <PRODUCT> <PRICE> <NUM OF ITEMS> <OFFER PRICE>. ex. A 30 3 70");
         System.out.println("- Enter checkout command in the format of space separated product codes. ex. A B C A D");
