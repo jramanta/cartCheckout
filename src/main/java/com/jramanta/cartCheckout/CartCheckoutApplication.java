285b675fb30f268870f6996945dde2647d795166
Ioanna Ramanta <jramanta@gmail.com>
2018-03-18 19:50:18 +0000

    Implementation of the main method and addition of initialization file

--- src/main/java/com/jramanta/cartCheckout/CartCheckoutApplication.java
+++ src/main/java/com/jramanta/cartCheckout/CartCheckoutApplication.java
@@ -1,8 +1,18 @@
 package com.jramanta.cartCheckout;
 
+import com.jramanta.cartCheckout.model.Product;
+import com.jramanta.cartCheckout.services.InitializeService;
+import com.jramanta.cartCheckout.services.ScannerService;
+
+import java.util.Map;
+
 public class CartCheckoutApplication {
 
-    public static void main(String args[]) {
+    private static final String INITIALIZE_FILE_PATH = "/initialization";
 
+    public static void main(String args[]) {
+        Map<String, Product> availableProducts = InitializeService
+                .initializeProductMap(INITIALIZE_FILE_PATH);
+        ScannerService.processInput(availableProducts);
     }
 }
